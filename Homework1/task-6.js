434a7fb6cd68f2aa4cfa1af6109f8cbf996b955c
olegtsilmak <92366057+olegtsilmak@users.noreply.github.com>
2022-02-18 10:33:57 +0200

    Add files via upload

---
+++ Homework1/task-6.js
@@ -0,0 +1,31 @@
+let firstName;
+let secondName;
+let studyGroup;
+let yearOfBirth;
+firstName = "Oleg";
+secondName = "Tsilmak";
+studyGroup = 678;
+yearOfBirth = 1982;
+let maritalStatus = true;
+
+console.log(typeof firstName);
+console.log(typeof secondName);
+console.log(typeof studyGroup);
+console.log(typeof yearOfBirth);
+console.log(typeof maritalStatus);
+
+console.log(studyGroup);
+console.log(yearOfBirth);
+console.log(maritalStatus);
+console.log(firstName);
+console.log(secondName);
+
+let myCar = null;
+console.log(typeof myCar);
+
+let userNoName;
+console.log(typeof userNoName);
+
+
+
+
